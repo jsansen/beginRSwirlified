0d52b04f9f0b7f180645e43c52aa35f51a7db323
maitre_joris <jorissansen@gmail.com>
2021-07-26 11:44:36 +0200

    minor fixes

--- Fonctions/scripts/evaluate-correct.R
+++ Fonctions/scripts/evaluate-correct.R
@@ -7,11 +7,11 @@
 # multiply_two_numbers = function(num1, num2){
 #	num1 * num2
 # }
-#
+
 # some_function = function(func){
 #    func(2, 4)
 # }
-#
+
 # Comme vous pouvez le voir, nous utilisons l'argument 'func' comme une fonction dans le corps de la fonction "some_function".
 
 # En passant la fonction add_two_number en argument à la fonction some_function (comme ceci: "some_function(add_two_numbers)" ) La fonction retournera 6.
@@ -19,7 +19,7 @@
 
 # Terminez la définition de la fonction ci-dessous afin que lorsqu'on lui passe une fonction en argument 'func' et des données (comme un vecteur) pour l'argument 'dat', la fonction evaluate retournera le resultat de func avec dat en argument.
 
-# astuces: bien que simpliste, cete exercice est compliqué si on ne comprend pas la tâche attendue, voici donc quelques exemple d'application et de la réponse attendue:
+# astuces: bien que simpliste, cet exercice est compliqué si on ne comprend pas la tâche attendue, voici donc quelques exemple d'application et de la réponse attendue:
 
 #    1. evaluate(sum, c(2, 4, 6)) retournera 12
 #    2. evaluate(median, c(7, 40, 9)) retournera 9
